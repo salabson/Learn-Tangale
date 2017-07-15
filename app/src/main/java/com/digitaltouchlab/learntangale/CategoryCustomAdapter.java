9742044ed096d95157cfb4ca06c2f96555f6d2cf
Salabson <salabson4@yahoo.co.uk>
2017-07-15 16:47:00 +0100

    Rename list_item.xml to category_list_item

--- app/src/main/java/com/digitaltouchlab/learntangale/CategoryCustomAdapter.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/CategoryCustomAdapter.java
@@ -1,7 +1,6 @@
 package com.digitaltouchlab.learntangale;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -34,7 +33,7 @@ public class CategoryCustomAdapter extends ArrayAdapter<WordCategory> {
         // check if there is view for reuse ortherwise inflate it
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_item,null);
+            convertView = inflater.inflate(R.layout.category_list_item,null);
         }
 
         // get references of various views in the inflated layout
