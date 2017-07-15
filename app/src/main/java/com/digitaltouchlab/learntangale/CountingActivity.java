80923712cf56722c4b2d120500ba9e42bf5e6cc3
Salabson <salabson4@yahoo.co.uk>
2017-07-15 16:44:21 +0100

    Delete activities xml files so that they use common layout activity_word.xml

--- app/src/main/java/com/digitaltouchlab/learntangale/CountingActivity.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/CountingActivity.java
@@ -8,6 +8,6 @@ public class CountingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counting);
+        setContentView(R.layout.activity_word);
     }
 }
