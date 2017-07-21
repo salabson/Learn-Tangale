81ae08a5c0684d898c352c5081f83117f33ea0e2
Salabson <salabson4@yahoo.co.uk>
2017-07-21 09:59:31 +0100

    Add addFavorite and removeFavorite methods in SharedPreferences class

--- app/src/main/java/com/digitaltouchlab/learntangale/SharedPrefrence.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/SharedPrefrence.java
@@ -3,7 +3,9 @@ package com.digitaltouchlab.learntangale;
 import android.content.Context;
 import android.content.SharedPreferences;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,15 +22,20 @@ public class SharedPrefrence {
     }
 
     // save data to shared prefrences
-    public void saveFavorites(Context context, List<Word> favorites) {
+    private void saveFavorites(Context context, List<Word> favorites) {
         // get reference to share preference object
         SharedPreferences sharedPrefs = context.getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPrefs.edit();
 
         // create Gson object
         Gson gson = new Gson();
+
+        // get type of the list of word object
+        Type type = new TypeToken<List<Word>>() {}.getType();
+
         // convert favorites list to json object
-        String jsonFavorites = gson.toJson(favorites);
+        String jsonFavorites = gson.toJson(favorites,type);
+
 
         // save the json object to sharedPrefereces
         editor.putString(FAVORITES, jsonFavorites);
@@ -36,24 +43,43 @@ public class SharedPrefrence {
     }
 
     // retrieve data from shared preferences
-    public List<Word> getFavorite(Context context, Word word) {
+    private List<Word> getFavorite(Context context, Word word) {
         // get reference to share preference object
         SharedPreferences sharedPrefs = context.getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE);
         List<Word> favorites;
+
+        // check if the specific key exist in the share
         if (sharedPrefs.contains(FAVORITES)) {
-            // get Json data from shared preferences
+            // get Json string from shared preferences
             String jsonFavorites = sharedPrefs.getString(FAVORITES,null);
             // create Gson object
             Gson gson = new Gson();
-            // convert favorites list to json object
-            Word[] jsonFavoritesItems = gson.fromJson(jsonFavorites, Word[].class);
-
-            favorites = Arrays.asList(jsonFavoritesItems);
-            favorites = new ArrayList<Word>(favorites);
+            // get type of the list of word object
+            Type type = new TypeToken<List<Word>>() {}.getType();
+            // convert favorites list from json to array of word object
+            favorites = gson.fromJson(jsonFavorites, type);
 
         } else return null;
 
         return favorites;
+    }
+
+    // remove item from the favorite
+    public void removeFavorite(Context context, Word word) {
+        List<Word> favorites = getFavorite(context, word);
+        if (favorites != null) {
+            favorites.remove(word);
+            saveFavorites(context,favorites);
+        }
+    }
+
+
+    public void addFavorite(Context context, Word word) {
+        List<Word> favorites = getFavorite(context, word);
+        if (!favorites.contains(word)) {
+            favorites.add(word);
+            saveFavorites(context,favorites);
+        }
     }
 
 
