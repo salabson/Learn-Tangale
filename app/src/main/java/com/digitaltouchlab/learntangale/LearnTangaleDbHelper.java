73471e8277f4f1184c698c24c65a47990263f0ff
Salabson <salabson4@yahoo.co.uk>
2017-07-23 13:04:52 +0100

    Create Db helper class and implement both methods

---
+++ app/src/main/java/com/digitaltouchlab/learntangale/LearnTangaleDbHelper.java
@@ -0,0 +1,51 @@
+package com.digitaltouchlab.learntangale;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.R.attr.version;
+
+/**
+ * Created by salabs on 23/07/2017.
+ */
+
+public class LearnTangaleDbHelper extends SQLiteOpenHelper{
+
+        // the database name
+    private static final String DATABASE_NAME = "learntangale.db";
+
+    // you must channge this if you change the schema of the db
+    private static int  DATABASE_VERSION = 1;
+
+    public LearnTangaleDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // create table to hold learn tangale data
+        final String SQL_CREATE_LEARN_TANGALE_TABLE = "CREATE TABLE " + LearnTangaleContract.LearnTangaleEntry.TABLE_NAME + " ("
+                 + LearnTangaleContract.LearnTangaleEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                 + LearnTangaleContract.LearnTangaleEntry.COLUMN_ENGLISH + " TEXT NOT NULL, "
+                 + LearnTangaleContract.LearnTangaleEntry.COLUMN_HAUSA + " TEXT NOT NULL, "
+                 + LearnTangaleContract.LearnTangaleEntry.COLUMN_IMAGEID + " INT NOT NULL"
+                 + LearnTangaleContract.LearnTangaleEntry.COLUMN_IS_ADDED_TO_FAVORITE + " BOOLEAN NOT NULL"
+                 + "); ";
+        // execute  the query against Db
+        sqLiteDatabase.execSQL(SQL_CREATE_LEARN_TANGALE_TABLE);
+
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        // drop the table if exist and recreate the table
+        sqLiteDatabase.execSQL("DROP TABLE IF EXIST " + LearnTangaleContract.LearnTangaleEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+
+    }
+}
