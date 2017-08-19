68450f4db9230c9dff480e3f7a68ca7d0c901bd6
Salabson <salabson4@yahoo.co.uk>
2017-08-19 22:05:45 +0100

    Implement onCreateOptionMenu and onOptionItemSelected for settings activity

--- app/src/main/java/com/digitaltouchlab/learntangale/SettingsActivity.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.digitaltouchlab.learntangale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.learn_tangale_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingIntent = new Intent(this,SettingsActivity.class);
+            startActivity(settingIntent);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
