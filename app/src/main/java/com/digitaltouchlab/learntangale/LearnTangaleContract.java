04876b72c51f42d8fa3ba52655f3f6a4836c8bfe
Salabson <salabson4@yahoo.co.uk>
2017-09-11 14:18:58 +0100

    Add CategoryEntry in the contract class

--- app/src/main/java/com/digitaltouchlab/learntangale/LearnTangaleContract.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/LearnTangaleContract.java
@@ -8,14 +8,22 @@ import android.provider.BaseColumns;
 
 public class LearnTangaleContract {
 
-    public static final class LearnTangaleEntry implements BaseColumns {
-        public static final String TABLE_NAME ="learntangle";
+    public static final class WordEntry implements BaseColumns {
+        public static final String TABLE_NAME ="word";
         public static final String COLUMN_TANGALE ="tangale";
         public static final String COLUMN_ENGLISH ="english";
         public static final String COLUMN_HAUSA ="hausa";
         public static final String COLUMN_IMAGEID ="imageId";
         public static final String COLUMN_IS_ADDED_TO_FAVORITE ="isAddedToFavorite";
+        public static final String COLUMN_CATEGORYID ="categoryId";
 
     }
+
+    public static final class CategoryEntry implements BaseColumns {
+        public static final String TABLE_NAME ="category";
+        public static final String COLUMN_NAME ="name";
+        public static final String COLUMN_IMAGEID ="imageId";
+    }
+
 
 }
