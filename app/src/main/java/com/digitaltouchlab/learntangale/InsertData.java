e5e2f2cf9103aa01d079c4496b193804df6d9ec1
Salabson <salabson4@yahoo.co.uk>
2017-09-11 21:51:24 +0100

    Finish updating codes in InsertData class

--- app/src/main/java/com/digitaltouchlab/learntangale/InsertData.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/InsertData.java
@@ -30,65 +30,45 @@ public class InsertData {
 
         ContentValues cv = new ContentValues();
         cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Animals");
-        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Animals");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.animals);
 
         list.add(cv);
 
         cv = new ContentValues();
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_TANGALE,"Babba");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_ENGLISH, "Monkey");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_HAUSA,"Jaki");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IMAGE_ID, R.drawable.donkey);
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IS_ADDED_TO_FAVORITE, "true");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_CATEGORY_ID,1);
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Family");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.family);
         list.add(cv);
 
         cv = new ContentValues();
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_TANGALE,"Ankilin");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_ENGLISH, "Lizard");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_HAUSA,"Kadangare");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IMAGE_ID, R.drawable.lizard);
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IS_ADDED_TO_FAVORITE, "false");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_CATEGORY_ID,1);
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Counting");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.numbers);
         list.add(cv);
 
 
         cv = new ContentValues();
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_TANGALE,"Tuje");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_ENGLISH, "Horse");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_HAUSA,"Doki");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IMAGE_ID, R.drawable.horse);
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IS_ADDED_TO_FAVORITE, "true");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_CATEGORY_ID,1);
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Enquiries");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.question);
         list.add(cv);
 
 
         cv = new ContentValues();
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_TANGALE,"Tanga");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_ENGLISH, "Cow");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_HAUSA,"Shanu");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IMAGE_ID, R.drawable.cow);
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IS_ADDED_TO_FAVORITE, "false");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_CATEGORY_ID,1);
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Phrases");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.phrases);
         list.add(cv);
 
 
         cv = new ContentValues();
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_TANGALE,"Andokko");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_ENGLISH, "Grasshopper");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_HAUSA,"Fara");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IMAGE_ID, R.drawable.grassphopper);
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_IS_ADDED_TO_FAVORITE, "true");
-        cv.put(LearnTangaleContract.WordEntry.COLUMN_CATEGORY_ID,1);
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_NAME,"Time and Seasons");
+        cv.put(LearnTangaleContract.CategoryEntry.COLUMN_IMAGE_ID,R.drawable.time);
         list.add(cv);
 
         try {
             db.beginTransaction();
             // clear db
-            db.delete(LearnTangaleContract.WordEntry.TABLE_NAME,null,null);
+            db.delete(LearnTangaleContract.CategoryEntry.TABLE_NAME,null,null);
             // loop to insert data in the list
             for (ContentValues c : list) {
-                db.insert(LearnTangaleContract.WordEntry.TABLE_NAME,null,c);
+                db.insert(LearnTangaleContract.CategoryEntry.TABLE_NAME,null,c);
             }
             db.setTransactionSuccessful();
             db.endTransaction();
