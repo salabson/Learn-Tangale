52ab4d7a6a4a6f20e99f380871c7c20d7d60c2fd
Salabson <salabson4@yahoo.co.uk>
2017-09-13 22:30:14 +0100

    Create getWordsByQueryString method in DatabaseUtils class

--- app/src/main/java/com/digitaltouchlab/learntangale/DatabaseUtils.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/DatabaseUtils.java
@@ -1,8 +1,10 @@
 package com.digitaltouchlab.learntangale;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.preference.PreferenceManager;
 
 /**
  * Created by salabs on 13/09/2017.
@@ -28,6 +30,7 @@ public class DatabaseUtils {
         mDbHelper.close();
     }
 
+    // get all data from the db as cursor
     public Cursor getAllword() {
         Cursor cursor = mDb.query(LearnTangaleContract.WordEntry.TABLE_NAME,
                 null,
@@ -39,8 +42,42 @@ public class DatabaseUtils {
                 null);
 
         return cursor;
+    }
 
+    // get selected data from db based on searchView query string
+    public Cursor getWordsByQueryString(String query) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String transLanguage = prefs.getString(mContext.getString(R.string.pref_translation_key),mContext.getString(R.string.pref_english_translation_value));
+        Cursor cursor = null;
+        // query db by english column
+        if (transLanguage.equals(mContext.getString(R.string.pref_english_translation_value))) {
+            //String[] selectionArgs = new String[]{"'%" + query + "%'"};
+            String selectionByEnglish = LearnTangaleContract.WordEntry.COLUMN_ENGLISH + " LIKE " + "'%" + query + "%'" ;
+            cursor = mDb.query(true,LearnTangaleContract.WordEntry.TABLE_NAME,
+                    null,
+                    selectionByEnglish,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+            //  query db by hausa column
+        } else if (transLanguage.equals(mContext.getString(R.string.pref_hausa_translation_value))) {
+            String selectionByHausa = LearnTangaleContract.WordEntry.COLUMN_HAUSA + " LIKE " + "'%" + query + "%'" ;
+            cursor = mDb.query(true,LearnTangaleContract.WordEntry.TABLE_NAME,
+                    null,
+                    selectionByHausa,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+        }
+        return cursor;
     }
 
 
 }
+
+
+
