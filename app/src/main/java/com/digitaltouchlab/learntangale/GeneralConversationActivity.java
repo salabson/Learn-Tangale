fcf8acd640825544e1db8dd234ad72db0212b75b
Salabson <salabson4@yahoo.co.uk>
2017-11-07 11:13:41 +0100

    Rename Phrases Actitvity ton General Conversation Activity

--- app/src/main/java/com/digitaltouchlab/learntangale/GeneralConversationActivity.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/GeneralConversationActivity.java
@@ -3,7 +3,7 @@ package com.digitaltouchlab.learntangale;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PhrasesActivity extends AppCompatActivity {
+public class GeneralConversationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/digitaltouchlab/learntangale/MainActivity.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/MainActivity.java
@@ -1,7 +1,6 @@
 package com.digitaltouchlab.learntangale;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -13,7 +12,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -130,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityIntent = new Intent(this,EnquiriesActivity.class);
                 break;
             case PHRASES:
-                startActivityIntent = new Intent(this,PhrasesActivity.class);
+                startActivityIntent = new Intent(this,GeneralConversationActivity.class);
                 break;
             case TIME_DATE:
                 startActivityIntent = new Intent(this,TimeActivity.class);
