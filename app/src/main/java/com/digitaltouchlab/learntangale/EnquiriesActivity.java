9d3acd7e87b52933d71d23682555937f09104a7c
Salabson <salabson4@yahoo.co.uk>
2017-11-07 11:20:13 +0100

    Code the Enquiries Activity

--- app/src/main/java/com/digitaltouchlab/learntangale/EnquiriesActivity.java
+++ app/src/main/java/com/digitaltouchlab/learntangale/EnquiriesActivity.java
@@ -1,13 +1,153 @@
 package com.digitaltouchlab.learntangale;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
-public class EnquiriesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class EnquiriesActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    ExpandableListView expLV;
+    List<Word> parentData;
+    HashMap<String, List<Word>> childData;
+    List<Word> words;
+    //hold each list for child data
+    List<Word> wordList;
+    // no item selected for the parent list view for the first time
+    private int lastSelectedItem = -1;
+    // database  access variables
+    DatabaseUtils mDbUtils;
+    SQLiteDatabase db;
+    LearnTangaleDbHelper dbHelper;
+    private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mDbUtils = new DatabaseUtils(this);
+
+
+        // make reference to expandable listview
+        expLV = new ExpandableListView(this);
+        expLV = (ExpandableListView) findViewById(R.id.exp_lv);
+        expLV.setGroupIndicator(null);
+        expLV.setDivider(getResources().getDrawable(R.color.lightGray));
+        expLV.setDividerHeight(1);
+
+        // initialize parent and child data viriables
+        parentData = new ArrayList<>();
+        childData = new HashMap<>();
+
+        // call method that populate parent and child data
+        mDbUtils.Open();
+        Cursor cursor = mDbUtils.getWordsByCategory(1);
+        LoadData.fillData(parentData, childData, cursor);
+
+        // create custom adapter object and set expandable list view to it
+        WordCustomAdapater customAdapater = new WordCustomAdapater(this, childData, parentData);
+        expLV.setAdapter(customAdapater);
+        // collapse one parent list view item when another is expanded
+        expLV.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+
+                if (lastSelectedItem != -1 && groupPosition != lastSelectedItem) {
+                    expLV.collapseGroup(lastSelectedItem);
+                }
+                lastSelectedItem = groupPosition;
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.learn_tangale_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent settingIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingIntent);
+            return true;
+        } else if (id == R.id.action_search) {
+            Intent searchIntent = new Intent(this, SearchActivity.class);
+            startActivity(searchIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDbUtils.Close();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+        mDbUtils.Open();
+        Cursor cursor = mDbUtils.getWordsByCategory(1);
+        parentData = new ArrayList<>();
+        childData = new HashMap<>();
+        LoadData.fillData(parentData, childData, cursor);
+        WordCustomAdapater customAdapater = new WordCustomAdapater(this, childData, parentData);
+        expLV.setAdapter(customAdapater);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String Key) {
+
+        // call method that populate parent and child data
+        mDbUtils.Open();
+        Cursor cursor = mDbUtils.getWordsByCategory(1);
+        parentData = new ArrayList<>();
+        childData = new HashMap<>();
+        LoadData.fillData(parentData, childData, cursor);
+
+
+        // create custom adapter object and set expandable list view to it
+        WordCustomAdapater customAdapater = new WordCustomAdapater(this, childData, parentData);
+        mDbUtils.Close();
+
     }
 }
